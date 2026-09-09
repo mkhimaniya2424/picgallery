562230c03243411b473ef7374bdc5d7dcb605d5a
Meghana_Khimaniya <mkhimaniya741@rku.aac.in>
2026-09-09 16:41:05 +0530

    commit

--- lib/screens/client/chat_thread_screen.dart
+++ lib/screens/client/chat_thread_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
-
 import '../../core/constants/app_constants.dart';
 import '../../core/theme/app_theme.dart';
 import '../../models/chat_message_model.dart';
